a6a456940678f1a8f63fec3edb50611218108439
Test1 <bluegamingpanda@gmail.com>
2017-11-05 14:24:03 +0200

    Added CauciucService

--- functions/src/ro/sci/domain/Cauciuc.java
+++ functions/src/ro/sci/domain/Cauciuc.java
@@ -2,9 +2,11 @@ package ro.sci.domain;
 
 
 public class Cauciuc {
-    public int marimeCauciuc;
-    public int diametruCauciuc;
-    public String tipCauciuc;
-    public String producatorCauciuc;
+    public String numeCauciuc;
+    public String sezon;
+    public int latime;
+    public int inaltime;
+    public int raza;
 
+    public Cauciuc(String modelCauciuc){this.numeCauciuc = modelCauciuc;}
 }
--- functions/src/ro/sci/service/CauciucService.java
+++ functions/src/ro/sci/service/CauciucService.java
@@ -1,4 +1,47 @@
 package ro.sci.service;
 
+import ro.sci.domain.Cauciuc;
+
+
 public class CauciucService {
+
+    private Cauciuc[] cauciucs;
+
+    public CauciucService() {initLocalCauciucs();}
+
+    public Cauciuc findCauciuc(String numeCauciuc){
+        Cauciuc foundCauciuc = null;
+        for(Cauciuc cauciuc: cauciucs) {
+            if (numeCauciuc != null && numeCauciuc.equals(cauciuc.numeCauciuc)) {
+                foundCauciuc = cauciuc;
+                break;
+            }
+        }
+        return foundCauciuc;
+
+    }
+
+
+    private void initLocalCauciucs() {
+
+    Cauciuc c1 = new Cauciuc("Kelly");
+    c1.sezon = "Toate";
+    c1.inaltime = 80;
+    c1.latime = 155;
+    c1.raza = 13;
+
+    Cauciuc c2 = new Cauciuc("Tiger");
+    c2.sezon = "Vara";
+    c2.inaltime = 65;
+    c2.latime = 155;
+    c2.raza = 13;
+
+    Cauciuc c3 = new Cauciuc("Debica");
+    c3.sezon = "Irarna";
+    c3.inaltime = 70;
+    c3.latime = 155;
+    c3.raza = 13;
+
+
+    }
 }
