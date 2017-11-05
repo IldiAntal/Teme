dd4299775e9b6c056325f654bb8b89ffc5e1af13
Test1 <bluegamingpanda@gmail.com>
2017-11-05 12:59:59 +0200

    Added relevant fields for Cauciuc

---
+++ functions/src/ro/sci/domain/Cauciuc.java
@@ -0,0 +1,10 @@
+package ro.sci.domain;
+
+
+public class Cauciuc {
+    public int marimeCauciuc;
+    public int diametruCauciuc;
+    public String tipCauciuc;
+    public String producatorCauciuc;
+
+}
